91498a5bb5bbe4d68c2d7ff5be2d49ba7974a8e2
andualem23 <andualem2326@gmail.com>
2023-06-11 10:07:06 +0300

    protect the some links

--- bug_tracker/dashboard/views.py
+++ bug_tracker/dashboard/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
+
 # Create your views here.
 
 
-class DashBoardView(TemplateView):
+class DashBoardView(LoginRequiredMixin, TemplateView):
+    login_url = "users/login"
+    redirect_field_name = ""
     template_name = "dashboard/index.html"
