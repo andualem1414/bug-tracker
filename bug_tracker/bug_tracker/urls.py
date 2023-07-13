3a832b10b360a102b92e03dd2295979c2803baf4
andualem23 <andualem2326@gmail.com>
2023-07-13 08:46:21 +0300

    check

--- bug_tracker/bug_tracker/urls.py
+++ bug_tracker/bug_tracker/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path("projects/", include("projects.urls")),
     path("tickets/", include("tickets.urls")),
     path("users/", include("users.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
